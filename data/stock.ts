9406cd86f7722995e1e8ff472b35eda8dcc7b37d
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-11-17 21:10:21 -0500

    test: add console log to test fetchLastClose data

--- data/stock.ts
+++ data/stock.ts
@@ -15,6 +15,7 @@ export async function fetchLastClose({
       `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev?adjusted=true&apiKey=${process.env.NEXT_PUBLIC_POLYGON_API_KEY}`
     );
     const data = await response.json();
+    console.log("fetchLastClose data", data);
     return { lastClose: data.results?.[0]?.c };
   } catch (error) {
     console.error("Error fetching last close:", error);
